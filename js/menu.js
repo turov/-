a799e18f9543c39f254a451815191c42997c744b
turov <ilya.turov@gmail.com>
2018-06-08 01:49:23 +0600

    scroll fix ☃️

--- js/menu.js
+++ js/menu.js
@@ -3,8 +3,6 @@
   let navToggle = document.querySelector('.menu-toggle');
   let wrapperBlack = document.querySelector('.wrapper-black');
 
-  navMain.style.paddingRight = navMain.offsetWidth - navMain.clientWidth + "px";
-
   navToggle.addEventListener('click', function () {
     navMain.classList.toggle('nav--opened');
     wrapperBlack.classList.toggle('wrapper-black--active');
